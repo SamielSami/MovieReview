ef0e6c9d140e734b1c988aa221fa247daf304c92
Sadia Tasnim Fariha <sadia21.official@gmail.com>
2025-08-30 00:21:32 +0600

    Adding Personal Lists feature

--- authy/views.py
+++ authy/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from django.contrib.auth.models import User
-from authy.models import Profile
+from authy.models import Profile, PersonalList
 from movie.models import Movie, Review, Likes
 
 from comment.models import Comment
@@ -18,6 +18,8 @@ from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.core.paginator import Paginator
+from django.contrib import messages
+
 # Create your views here.
 
 
@@ -102,6 +104,7 @@ def UserProfile(request, username):
 	sWatched_count = profile.watched.filter(Type='series').count()
 	watch_list_count = profile.to_watch.all().count()
 	m_reviewd_count = Review.objects.filter(user=user).count()
+	personal_list_count = PersonalList.objects.filter(user=user).count()
 
 
 	context = {
@@ -110,6 +113,7 @@ def UserProfile(request, username):
 		'sWatched_count': sWatched_count,
 		'watch_list_count': watch_list_count,
 		'm_reviewd_count': m_reviewd_count,
+		'personal_list_count': personal_list_count,
 	}
 
 	template = loader.get_template('profile.html')
@@ -213,3 +217,92 @@ def UserProfileWatchList(request, username):
 	template = loader.get_template('profile.html')
 
 	return HttpResponse(template.render(context, request))
+
+@login_required
+def personal_lists(request, username):
+	user = get_object_or_404(User, username=username)
+	if request.user != user:
+		return HttpResponseRedirect(reverse('user-profile', args=[request.user.username]))
+
+	if request.method == 'POST':
+		name = request.POST.get('name', '').strip()
+		is_private = bool(request.POST.get('is_private'))
+		if name:
+			if PersonalList.objects.filter(user=user).count() >= 5:
+				messages.error(request, 'You have reached the maximum of 5 personal lists.')
+				return HttpResponseRedirect(reverse('personal-lists', args=[username]))
+			PersonalList.objects.get_or_create(user=user, name=name, defaults={'is_private': is_private})
+			messages.success(request, 'Personal list created.')
+		return HttpResponseRedirect(reverse('personal-lists', args=[username]))
+
+	lists = PersonalList.objects.filter(user=user)
+	context = {
+		'profile': user.profile,
+		'lists': lists,
+	}
+	return render(request, 'personal_lists.html', context)
+
+
+@login_required
+def delete_personal_list(request, list_id):
+	plist = get_object_or_404(PersonalList, id=list_id)
+	if plist.user != request.user:
+		return HttpResponseRedirect(reverse('personal-lists', args=[request.user.username]))
+	plist.delete()
+	messages.success(request, 'Personal list deleted.')
+	return HttpResponseRedirect(reverse('personal-lists', args=[request.user.username]))
+
+
+@login_required
+def personal_list_detail(request, username, list_id):
+	user = get_object_or_404(User, username=username)
+	plist = get_object_or_404(PersonalList, id=list_id, user=user)
+	if plist.is_private and request.user != user:
+		messages.error(request, 'This list is private.')
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+	movies = plist.movies.all()
+	context = {
+		'profile': user.profile,
+		'plist': plist,
+		'movies': movies,
+	}
+	return render(request, 'personal_list_detail.html', context)
+
+
+@login_required
+def add_to_personal_list(request, imdb_id):
+	if not validate_imdb_id(imdb_id):
+		return HttpResponseRedirect(reverse('index'))
+	movie = get_object_or_404(Movie, imdbID=imdb_id)
+	profile = request.user.profile
+	if not profile.watched.filter(id=movie.id).exists():
+		messages.error(request, 'You can only add movies you have marked as watched to personal lists.')
+		return HttpResponseRedirect(reverse('movie-details', args=[imdb_id]))
+
+	if request.method == 'POST':
+		list_ids = request.POST.getlist('list_ids')
+		added = 0
+		for lid in list_ids:
+			plist = PersonalList.objects.filter(id=lid, user=request.user).first()
+			if plist:
+				plist.movies.add(movie)
+				added += 1
+		if added:
+			messages.success(request, 'Movie added to selected list(s).')
+		return HttpResponseRedirect(reverse('movie-details', args=[imdb_id]))
+
+	lists = PersonalList.objects.filter(user=request.user)
+	context = {
+		'movie': movie,
+		'lists': lists,
+	}
+	return render(request, 'choose_personal_list.html', context)
+
+
+@login_required
+def remove_from_personal_list(request, list_id, imdb_id):
+	plist = get_object_or_404(PersonalList, id=list_id, user=request.user)
+	movie = get_object_or_404(Movie, imdbID=imdb_id)
+	plist.movies.remove(movie)
+	messages.success(request, 'Movie removed from the list.')
+	return HttpResponseRedirect(reverse('personal-list-detail', args=[request.user.username, plist.id]))
